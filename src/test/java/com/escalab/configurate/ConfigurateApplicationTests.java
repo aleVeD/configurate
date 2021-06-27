a342a227a81ce897efd0adce71e4b5a659e185aa
aleDVM <pellahuenco@gmail.com>
2021-06-27 14:31:18 -0400

    Initial commit

---
+++ src/test/java/com/escalab/configurate/ConfigurateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escalab.configurate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
